7db94f95d7369083de4f087600e78f841a0fa01a
Nisharg-K <2203031280012@paruluniversity.acc.in>
2025-10-08 03:33:16 +0530

    CAM1

--- cam.py
+++ cam.py
@@ -10,10 +10,7 @@ import serial.tools.list_ports
 import time
 import sys
 
-# --- Configuration ---
-# IMPORTANT: Change this to your device's serial port on the Raspberry Pi.
-# It will likely be '/dev/ttyACM0' or '/dev/ttyUSB0'.
-# Run 'ls /dev/tty*' in your terminal to find the correct port.
+
 SERIAL_PORT = '/dev/ttyUSB0'
 BAUD_RATE = 115200
 
